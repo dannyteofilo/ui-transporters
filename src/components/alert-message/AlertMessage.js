a1456e233802b72047bb0694d31e41bae4abc074
danny <danny@linux-0sc2.suse>
2018-08-25 16:38:34 -0500

    implementation alert message

---
+++ src/components/alert-message/AlertMessage.js
@@ -0,0 +1,14 @@
+import Reatc, { Component } from 'react';
+import Sweet from 'sweetalert2'
+
+class AlertMessage extends Component {
+    render() {
+        return (
+            <div>
+                altertMessage Component
+            </div>
+        )
+    }
+}
+
+export default AlertMessage;
---
+++ src/modules/profile/Profile.js
@@ -0,0 +1,14 @@
+import React , {component} from 'react';
+
+
+class UserProfile extends Component{
+    render(){
+        return(
+            <div>
+                UserProfile Component
+            </div>
+        )
+    }
+}
+
+export default UserProfile;
